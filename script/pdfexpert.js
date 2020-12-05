5b08ee46f8681fa2e840b7c91f4569e67b78eebb
Charstr <39878604+Charstr@users.noreply.github.com>
2020-12-05 16:54:07 +0800

    Create pdfexpert.js

---
+++ script/pdfexpert.js
@@ -0,0 +1,17 @@
+/*
+[Script]
+^https:\/\/license\.pdfexpert\.com\/api\/.*\/(documents|pdfexpert6)\/subscription\/(refresh$|check$) url script-response-body Documents&PDFExpert.js
+
+[MITM]
+hostname = license.pdfexpert.com
+*/
+var obj = JSON.parse($response.body);
+obj["isInGracePeriod"] = false;
+obj["isEligibleForIntroPeriod"] = false;
+obj["originalTransactionId"] = "20000000000000";
+obj["subscriptionState"] = "active";
+obj["subscriptionExpirationDate"] = "23:59 31/12/2029";
+obj["subscriptionAutoRenewStatus"] = "autoRenewOff";
+obj["subscriptionReceiptId"] = "1500000000000";
+
+$done({body: JSON.stringify(obj)});
