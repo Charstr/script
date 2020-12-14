0436c44c1e6e9798ebe9095cd85066f73db02af1
Charstr <39878604+Charstr@users.noreply.github.com>
2020-12-14 15:47:57 +0800

    Create itranslate.js

---
+++ script/itranslate.js
@@ -0,0 +1,28 @@
+/*
+[Script]
+^https:\/\/ssl-api\.itranslateapp\.com\/accounts\/v4\/subscriptions\/verify\/ios
+[MITM]
+hostname = ssl-api.itranslateapp.com
+*/
+
+let body = JSON.parse($response.body)
+obj.licenses = [
+    {
+      "bundle_id": "com.outerspaceapps.itranslate",
+      "expires_date_ms": 2608467875000,
+      "is_trial_period": true,
+      "original_transaction_id": "480000745188088",
+      "product_id": "com.itranslate.pro.yearlytrial",
+      "transaction_id": "480000745188088"
+    },
+    {
+      "bundle_id": "com.outerspaceapps.itranslate",
+      "expires_date_ms": 2608467875000,
+      "is_trial_period": true,
+      "original_transaction_id": "480000745188088",
+      "product_id": "com.itranslate.pro.yearlytrial",
+      "transaction_id": "480000745188088"
+    }
+  ]
+body = JSON.stringify(body)
+$done({body})
