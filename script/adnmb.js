11c705f49980028875c992309c83ec8dfd22d1d0
Charstr <39878604+Charstr@users.noreply.github.com>
2021-01-05 13:28:56 +0800

    Update adnmb.js

--- script/adnmb.js
+++ script/adnmb.js
@@ -1,8 +1,20 @@
 /*
 host = nmb.fastmirror.org
-^https:\/\/nmb\.fastmirror\.org\/Api\/showf
+
+^https?://nmb\.fastmirror\.org/Api/(thread|showf)
 */
-let body = JSON.parse($response.body)
-body[0] = '';
-body = JSON.stringify(body)
+const path1 = "/Api/showf"
+const path2 = "/Api/thread"
+
+const url = $request.url;
+let body = $response.body;
+if(url.indelOf(path1) != -1){
+    body[0] = '';
+    body = JSON.stringify(body)
+}
+if(url.indelOf(path2) != -1){
+    body.reply[0] = '';
+    body = JSON.stringify(body)
+}
+
 $done({body})
