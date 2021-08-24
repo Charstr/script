5feeb903270b61f868c85c1bc670a2967ee04453
Charstr <39878604+Charstr@users.noreply.github.com>
2021-08-24 18:01:46 +0800

    Update jrrb.js

--- script/jrrb.js
+++ script/jrrb.js
@@ -6,7 +6,7 @@ https://api2.tophub.app/account/sync
 let body = JSON.parse($response.body)
 body.data = {
     "is_vip": "1",
-    "vip_expired": "3603191721",
+    "vip_expired": "4603191721",
       "is_vip_now": true
     };
 body = JSON.stringify(body)
