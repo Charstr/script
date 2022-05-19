075979175ef2f50df7238c598806c4872693571e
Charstr <39878604+Charstr@users.noreply.github.com>
2022-05-19 10:44:48 +0800

    Update pdfexpert.js

--- script/pdfexpert.js
+++ script/pdfexpert.js
@@ -4,28 +4,51 @@
 [MITM]
 hostname = license.pdfexpert.com
 */
-var obj= {
-	"bundleId": "com.readdle.PDFExpert5",
-	"chargingPlatform": "iOS AppStore",
-	"receiptId": 1650886513000,
-	"originalTransactionId": 550001075931630,
-	"inAppStates": [{
-		"type": "subscription",
-		"productId": "com.readdle.PDFExpert5.subscription.month10",
-		"originalTransactionId": 550001075931630,
-		"productName": "subscription",n
-		"isEligibleForIntroPeriod": false,
-		"subscriptionExpirationDate": "12:52 02/06/2092",
-		"subscriptionState": "active",
-		"subscriptionAutoRenewStatus": "autoRenewOn",
-		"isInGracePeriod": false,
-		"isInBillingRetryPeriod": false,
-		"entitlements": ["ios.pe.subscription.pdf-features"]
-	}],
-	"receiptStatus": "ok",
-	"statisticsInfo": {
-		"events": []
-	}
-}
+var obj={
+  "productId": "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user",
+  "subscriptionExpirationIntent": "userCancelled",
+  "receiptStatus": "ok",
+  "subscriptionExpirationDate": "17:48 25/11/2099",
+  "inAppStates": [
+    {
+      "productId": "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user",
+      "subscriptionExpirationIntent": "userCancelled",
+      "receiptStatus": "ok",
+      "subscriptionExpirationDate": "17:48 25/11/2019",
+      "isEligibleForIntroPeriod": false,
+      "originalTransactionId": "20000625420102",
+      "productName": "subscription",
+      "isInBillingRetryPeriod": false,
+      "type": "subscription",
+      "subscriptionState": "active",
+      "subscriptionAutoRenewStatus": "autoRenewOff",
+      "isInGracePeriod": false
+    },
+    {
+      "originalTransactionId": "0000",
+      "entitlements": [
+      ],
+      "type": "custom purchase",
+      "productId": "documents6-user"
+    }
+  ],
+  "isDocuments6User": true,
+  "isEligibleForIntroPeriod": false,
+  "originalTransactionId": "20000625420102",
+  "isEligibleFor": [
+    "winback"
+  ],
+  "isInBillingRetryPeriod": false,
+  "type": "subscription",
+  "inAppPurchased": [
+    "com.readdle.ReaddleDocsIPad.subscription.year50_upd_user"
+  ],
+  "bundleId": "com.readdle.ReaddleDocsIPad",
+  "receiptId": 1530908572000,
+  "chargingPlatform": "iOS AppStore",
+  "subscriptionState": "active",
+  "subscriptionAutoRenewStatus": "autoRenewOff",
+  "isInGracePeriod": false
+};
 
 $done({body: JSON.stringify(obj)});
