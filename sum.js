8f52b414c6ca379981679f33c053dfeb923378be
Charstr <39878604+Charstr@users.noreply.github.com>
2023-01-30 18:20:44 +0800

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,16 @@
+let body = $response.body; 
+if (body) { 
+    switch (!0) { 
+        case /^https:\/\/bbs-api\.miyoushe\.com\/misc\/api\/getSiteHotKeyword/.test($request.url):
+            cont = JSON.parse(body);
+            cont.data={};
+            body = JSON.stringify(cont);
+        break;
+        case /^https:\/\/bbs-api\.miyoushe\.com\/apihub\/api\/home\/new/.test($request.url): 
+            cont = JSON.parse(body);
+            cont.data.carousels={};
+            body = JSON.stringify(cont);
+        break;
+    }
+}
+$done({ body })
