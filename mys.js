ccef0ee212bd2bb401fe4f118c6ad8c6f8f51e05
= <=>
2023-12-06 22:30:28 +0800

    update

--- mys.js
+++ mys.js
@@ -1,17 +1,15 @@
 // 去除米游社搜索栏的推荐
-let body = $response.body; 
-if (body) { 
-    switch (!0) { 
-        case /^https:\/\/bbs-api\.miyoushe\.com\/misc\/api\/getSiteHotKeyword/.test($request.url):
-            cont = JSON.parse(body);
-            cont.data={};
-            body = JSON.stringify(cont);
-        break;
-        case /^https:\/\/bbs-api\.miyoushe\.com\/apihub\/api\/home\/new/.test($request.url): 
-            cont = JSON.parse(body);
-            cont.data.carousels={};
-            body = JSON.stringify(cont);
-        break;
+const body = $response.body;
+
+if (body) {
+    const url = $request.url;
+    const cont = JSON.parse(body);
+
+    if (/^https:\/\/bbs-api\.miyoushe\.com\/misc\/api\/getSiteHotKeyword/.test(url)) {
+        cont.data = {};
+    } else if (/^https:\/\/bbs-api\.miyoushe\.com\/apihub\/api\/home\/new/.test(url)) {
+        cont.data.carousels = {};
     }
+
+    $done({ body: JSON.stringify(cont) });
 }
-$done({ body })
